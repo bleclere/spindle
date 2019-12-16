01fcf7135fb57f6481b3491ffa6586d7305986d1
bleclere <brice.leclere@univ-nantes.fr>
2019-12-16 14:30:02 +0100

    initiate project

---
+++ outil-SPIn/server.R
@@ -0,0 +1,14 @@
+#           Projet : Outil-SPIn
+#           Auteur : BLE
+# Date de création : 16/12/19
+#            Objet : Server-side de l’outil de suivi des projets
+
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+
+  
+})
---
+++ outil-SPIn/ui.R
@@ -0,0 +1,15 @@
+#           Projet : Outil-SPIn
+#           Auteur : BLE
+# Date de création : 16/12/19
+#            Objet : Interface de l’outil de suivi des projets
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Gestion des projets"),
+  
+
+))
